62ad77e64282ab00c53fa13eb035bba68f5afe60
aquibzahidi <aquibzahidi@gmail.com>
2019-05-09 13:13:15 +0530

    update db conn 2

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 	// $conn = mysqli_connect("localhost","root","", "compsys");
-	$conn = mysqli_connect("crm.aligarhneeds.com","u388353699_crm","admin@123#", "u388353699_crm");
+	$conn = mysqli_connect("localhost","u388353699_crm","admin@123#", "u388353699_crm");
 	
 	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
