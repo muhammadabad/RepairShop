8a53358b766dd7b4efd46803902daf779f3e1c92
aquibzahidi <aquibzahidi@gmail.com>
2019-05-09 15:28:45 +0530

    update conn 3

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,11 @@
 <?php 
+	// $DB_HOST = 'localhost';
+	// $DB_USER = 'root';
+	// $DB_PASS = '';
+	// $DB_NAME = 'compsys';
 	$DB_HOST = 'localhost';
-	$DB_USER = 'root';
-	$DB_PASS = '';
-	$DB_NAME = 'compsys';
+	$DB_USER = 'u388353699_crm';
+	$DB_PASS = 'admin@123#';
+	$DB_NAME = 'u388353699_crm';
 	$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
